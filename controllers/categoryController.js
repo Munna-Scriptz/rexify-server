75f00359438b508c45bfbcf21813fe26650c0232
Munna <munnascriptz@gmail.com>
2026-01-26 23:51:44 +0600

    Category created and send to DB and uploaded to coudinary create category complete

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,3 +1,4 @@
+const categorySchema = require("../models/categorySchema")
 const { cloudUpload } = require("../services/cloudUpload")
 const resHandler = require("../utils/resHandler")
 
@@ -7,15 +8,23 @@ const createCategory = async (req, res) => {
         const thumbnail = req.file
 
         // ---------- Validations 
-        console.log(name, description, thumbnail)
+        const existingCategory = await categorySchema.findOne({name})
+        if (existingCategory) return resHandler.error(res, 400, "Category already exists")
         if (!name) return resHandler.error(res, 400, "Category name is required")
         if (!description) return resHandler.error(res, 400, "Category description is required")
         if (!thumbnail) return resHandler.error(res, 400, "Category thumbnail is required")
 
         // ---------- Upload image to cloudinary
-        const cloudeRes = await cloudUpload({file: thumbnail, folderPath: "rexify/categories", folder: "category"})
+        const cloudinaryRes = await cloudUpload({ file: thumbnail, folderPath: "rexify/categories", folder: "category" })
 
-        console.log(cloudeRes) 
+        // ----------- Send to DB 
+        const category = await categorySchema({
+            name,
+            description,
+            thumbnail: cloudinaryRes.secure_url
+        })
+
+        category.save()
 
         // --------------- Success
         resHandler.success(res, "Category created successfully")
