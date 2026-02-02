4b0d290dca13ec6db737196acb725007cb5e280f
Munna <munnascriptz@gmail.com>
2026-02-02 23:16:06 +0600

    Product: Hw-21 complete

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,3 +1,4 @@
+const productSchema = require("../models/productSchema")
 const { cloudUpload } = require("../services/cloudUpload")
 const resHandler = require("../utils/resHandler")
 
@@ -5,29 +6,42 @@ const createProduct = async (req, res) => {
     try {
         const { title, description, category, price, discountPercentage, variants, tags, isActive } = req.body
         const thumbnail = req.files?.thumbnail?.[0]
-        const images = req.files.images
+        const images = req.files?.images
 
         // ---------- Validation ----------
         if (!title) return resHandler.error(res, 400, 'Title is required')
         if (!description) return resHandler.error(res, 400, 'Description is required')
         if (!category) return resHandler.error(res, 400, 'Category is required')
         if (!price) return resHandler.error(res, 400, 'Price is required')
+        if (!thumbnail) return resHandler.error(res, 400, 'Product thumbnail is required')
 
         // ---------- Upload images ----------
-        const uploadRes = await cloudUpload({ file: thumbnail, folderPath: "rexify/products", folder: "product" })
+        const thumbRes = await cloudUpload({ file: thumbnail, folderPath: "rexify/products", folder: "product" })
 
+        const imageUrls = []
         if (images) {
             for (const img of images) {
                 const uploadRes = await cloudUpload({ file: img, folderPath: "rexify/products", folder: "product" })
-                console.log(uploadRes) 
-            } 
+                imageUrls.push(uploadRes.secure_url)
+            }
         }
- 
+
+        // ---------- Save to DB ----------
+
+        const product = productSchema({
+            title,
+            description,
+            category,
+            price,
+            discountPercentage,
+            images: imageUrls,
+            thumbnail: thumbRes.secure_url
+        })
+        product.save()
 
         // ------------ Success 
         res.send("Created Successfully")
     } catch (error) {
-        console.log(error)
         resHandler.error(res, 500, 'Internal Server Error')
     }
 }
