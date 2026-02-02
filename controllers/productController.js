eff443ab147bf47fcbca5e15014ec219236ec877
Munna <munnascriptz@gmail.com>
2026-02-02 22:16:27 +0600

    Product: Single and multiple image uploaded to cloudinary

--- controllers/productController.js
+++ controllers/productController.js
@@ -8,18 +8,18 @@ const createProduct = async (req, res) => {
         const images = req.files.images
 
         // ---------- Validation ----------
-        // if (!title) return resHandler.error(res, 400, 'Title is required')
-        // if (!description) return resHandler.error(res, 400, 'Description is required')
-        // if (!category) return resHandler.error(res, 400, 'Category is required')
-        // if (!price) return resHandler.error(res, 400, 'Price is required')
+        if (!title) return resHandler.error(res, 400, 'Title is required')
+        if (!description) return resHandler.error(res, 400, 'Description is required')
+        if (!category) return resHandler.error(res, 400, 'Category is required')
+        if (!price) return resHandler.error(res, 400, 'Price is required')
 
         // ---------- Upload images ----------
-        // const uploadRes = await cloudUpload({ file: thumbnail, folderPath: "rexify/products", folder: "product" })
+        const uploadRes = await cloudUpload({ file: thumbnail, folderPath: "rexify/products", folder: "product" })
 
         if (images) {
             for (const img of images) {
                 const uploadRes = await cloudUpload({ file: img, folderPath: "rexify/products", folder: "product" })
-                console.log(uploadRes)
+                console.log(uploadRes) 
             } 
         }
  
