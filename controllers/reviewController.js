1fa4f35d230c2fdeae8546ab1c4e2e8dcfd98b98
Munna <munnascriptz@gmail.com>
2026-03-01 23:19:29 +0600

    view controller validation complete

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -3,7 +3,14 @@ const resHandler = require("../utils/resHandler")
 // ============ create review 
 const createReview = (req, res) => {
     try {
+        const { productId, rating, comment } = req.body
 
+        if (!productId) return resHandler.error(res, 400, 'product id is required')
+        if (!rating || rating < 1 || rating > 5) return resHandler.error(res, 400, 'rating is required and must be at between 1-5')
+
+
+        // ----------- Success 
+        resHandler.success(res, 200, "Product review added")
     } catch (error) {
         resHandler.error(res, 500, "Internal server error")
     }
