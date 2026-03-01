ba0cbb4d2d1c8e7dc3f8bc3092edf4d9f7d0406d
Munna <munnascriptz@gmail.com>
2026-03-01 23:34:25 +0600

    Product review create complete

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,14 +1,25 @@
+const reviewSchema = require("../models/reviewSchema")
 const resHandler = require("../utils/resHandler")
 
 // ============ create review 
-const createReview = (req, res) => {
+const createReview = async (req, res) => {
     try {
         const { productId, rating, comment } = req.body
+        const user = req?.user?._id
 
         if (!productId) return resHandler.error(res, 400, 'product id is required')
         if (!rating || rating < 1 || rating > 5) return resHandler.error(res, 400, 'rating is required and must be at between 1-5')
+        if (!user) return resHandler.error(res, 404, 'review user not found')
 
+        // ------------- Save to DB 
+        const newReview = await reviewSchema({
+            product: productId,
+            user,
+            rating,
+            comment
+        })
 
+        newReview.save()
         // ----------- Success 
         resHandler.success(res, 200, "Product review added")
     } catch (error) {
