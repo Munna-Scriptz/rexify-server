8333c6196ed765251ea178a82b0dec75e68912a0
Munna <munnascriptz@gmail.com>
2026-03-01 23:43:17 +0600

    review controller duplicate review prevented

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,5 +1,6 @@
 const reviewSchema = require("../models/reviewSchema")
 const resHandler = require("../utils/resHandler")
+const { ObjectId } = require('mongodb');
 
 // ============ create review 
 const createReview = async (req, res) => {
@@ -8,8 +9,12 @@ const createReview = async (req, res) => {
         const user = req?.user?._id
 
         if (!productId) return resHandler.error(res, 400, 'product id is required')
+        if (!ObjectId.isValid(productId)) return resHandler.error(res, 400, 'invalid object id')
         if (!rating || rating < 1 || rating > 5) return resHandler.error(res, 400, 'rating is required and must be at between 1-5')
         if (!user) return resHandler.error(res, 404, 'review user not found')
+
+        const existingReview = await reviewSchema.findOne({ user, product: productId })
+        if (existingReview) return resHandler.error(res, 400, 'review already created')
 
         // ------------- Save to DB 
         const newReview = await reviewSchema({
@@ -18,8 +23,8 @@ const createReview = async (req, res) => {
             rating,
             comment
         })
-
         newReview.save()
+
         // ----------- Success 
         resHandler.success(res, 200, "Product review added")
     } catch (error) {
