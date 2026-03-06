6a3ad0feac841adbc9d2fd100c5fce33473b0679
Munna <munnascriptz@gmail.com>
2026-03-06 22:20:18 +0600

    Cart schema updated

--- models/cartSchema.js
+++ models/cartSchema.js
@@ -1,7 +1,14 @@
 const mongoose = require("mongoose");
 
-const cartItemSchema = new mongoose.Schema(
-    {
+const cartSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "user",
+        required: true,
+        unique: true,
+    },
+
+    items: [{
         product: {
             type: mongoose.Schema.Types.ObjectId,
             ref: "Product",
@@ -22,37 +29,18 @@ const cartItemSchema = new mongoose.Schema(
             type: Number,
             required: true,
         },
+    }, { _id: false }
+    ],
+
+    totalItems: {
+        type: Number,
+        default: 0,
     },
-    { _id: false }
-);
 
-const cartSchema = new mongoose.Schema(
-    {
-        user: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "user",
-            required: true,
-            unique: true,
-        },
-
-        items: [cartItemSchema],
-
-        totalItems: {
-            type: Number,
-            default: 0,
-        },
-
-        totalPrice: {
-            type: Number,
-            default: 0,
-        },
-    },
-    { timestamps: true }
-);
+}, { timestamps: true });
 
 cartSchema.pre("save", function () {
     this.totalItems = this.items.reduce((acc, item) => acc + item.quantity, 0);
-    this.totalPrice = this.items.reduce((acc, item) => acc + item.quantity * item.price, 0);
 });
 
 module.exports = mongoose.model("cart", cartSchema);
