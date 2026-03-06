b1db6dc130861c6aee52768cd9a39015ad00ba4a
Munna <munnascriptz@gmail.com>
2026-03-06 22:34:18 +0600

    cart controller validation added and 50% complete

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -1,8 +1,30 @@
+const cartSchema = require("../models/cartSchema")
 const resHandler = require("../utils/resHandler")
 
-const createCart = (req, res) => {
+const createCart = async (req, res) => {
     try {
+        const user = req.user._id
+        const { product, sku, quantity } = req.body
 
+        // ------------ Validation 
+        if (!user) return resHandler.error(res, 400, "invalid request")
+        if (!product) return resHandler.error(res, 400, "product is required")
+        if (!sku) return resHandler.error(res, 400, "product sku is required")
+        if (!quantity || quantity < 1) return resHandler.error(res, 400, "quantity is required and must be at least 1")
+
+            // ---------- Save to DB 
+            const newCart = cartSchema({
+                user,
+                items: [{
+                    product: "",
+                    sku: "",
+                    quantity: "",
+                    subTotal: ""
+                }]
+            })
+
+        // ----------- Success 
+        resHandler.success(res, 201, "Cart Added", newCart)
     } catch (error) {
         resHandler.error(res, 500, "Internal server error")
     }
